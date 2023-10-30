4e04cf3a41f16daae37edef8d35bffee28d1e9fc
Farhan0003 <103404444+Farhan0003@users.noreply.github.com>
2023-10-31 01:39:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 /*============== toggle icon navbar =====================*/
 let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let navbar = document.querySelector('.navbar'); // Correct the selector
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-
 };
+
 
 /*============== scroll sections avtive link=====================*/
 let sections= document.querySelectorAll('section');
@@ -34,4 +34,46 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY >100);
 
+    
+/*==============Remove toggle icon navbar when click navbar link (scroll)=====================*/
+
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 };
+
+/*==============scroll reveal=====================*/
+
+ScrollReveal().reveal('.home-content, .heading', {
+  origin: 'top', // Corrected placement within the options object
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+});
+
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {
+    origin: 'bottom', // Corrected placement within the options object
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+  });
+  ScrollReveal().reveal('.home-content h1, .about-image', {
+    origin: 'left', // Corrected placement within the options object
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+  });
+  ScrollReveal().reveal('.home-content p, .about-content', {
+    origin: 'right', // Corrected placement within the options object
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+  });
+
+  /*==============typed js=====================*/
+  const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Graphics Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
